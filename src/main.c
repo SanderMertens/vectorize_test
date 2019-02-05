795147387c8f2ff07570b58f637df1ff23abd67f
Sander Mertens <sander.mertens8@gmail.com>
2019-02-04 22:52:03 -0800

    Measure both vectorized and non-vectorized variants of SoA and AoS

--- src/main.c
+++ src/main.c
@@ -34,12 +34,21 @@ void benchmark(int count) {
     float *y = malloc(count * sizeof(float));
     float *s = malloc(count * sizeof(float));
 
+    float *x_novec = malloc(count * sizeof(float));
+    float *y_novec = malloc(count * sizeof(float));
+    float *s_novec = malloc(count * sizeof(float));
+
+
     /* Store Position and Speed in separate arrays */
     Position *positions = malloc(count * sizeof(Position));
     Speed *speeds = malloc(count * sizeof(Speed));
 
+    Position *positions_novec = malloc(count * sizeof(Position));
+    Speed *speeds_novec = malloc(count * sizeof(Speed));
+
     /* Store entity structs in array */
     Entity *entities = malloc(count * sizeof(Entity));
+    Entity *entities_novec = malloc(count * sizeof(Entity));
 
     /* Store entities in separate blocks on the heap */
     Entity **entity_ptrs = malloc(count * sizeof(Entity*));
@@ -62,7 +71,9 @@ void benchmark(int count) {
     printf("-- Start benchmarks\n");
 
 
-    /* -- Attributes in separate arrays */
+
+    /* -- SoA (attributes) -- */
+
     timespec_gettime(&start);
 #pragma clang loop vectorize(enable)
     for(int i = 0; i < count; i ++) {
@@ -79,8 +90,27 @@ void benchmark(int count) {
     }
     printf("   SoA, warm:                    %f (V)\n", timespec_measure(&start));
 
+    /* No vectorization */
+    timespec_gettime(&start);
+#pragma clang loop vectorize(disable)
+    for(int i = 0; i < count; i ++) {
+        x_novec[i] += s_novec[i];
+        y_novec[i] += s_novec[i];
+    }
+    printf("   SoA, cold:                    %f\n", timespec_measure(&start));
 
-    /* -- Position in a separate struct */
+    timespec_gettime(&start);
+#pragma clang loop vectorize(disable)
+    for(int i = 0; i < count; i ++) {
+        x_novec[i] += s_novec[i];
+        y_novec[i] += s_novec[i];
+    }
+    printf("   SoA, warm:                    %f\n", timespec_measure(&start));
+
+
+
+    /* -- SoA (components) -- */
+
     timespec_gettime(&start);
 #pragma clang loop vectorize(enable)
     for(int i = 0; i < count; i ++) {
@@ -97,26 +127,65 @@ void benchmark(int count) {
     }
     printf("   SoA (components), warm:       %f (V)\n", timespec_measure(&start));
 
-
-    /* -- All attributes in one Entity struct (contains also other, unused data) */
+    /* No vectorization */
     timespec_gettime(&start);
 #pragma clang loop vectorize(disable)
     for(int i = 0; i < count; i ++) {
+        positions_novec[i].x += speeds_novec[i];
+        positions_novec[i].y += speeds_novec[i];
+    }
+    printf("   SoA (components), cold:       %f\n", timespec_measure(&start));
+
+    timespec_gettime(&start);
+#pragma clang loop vectorize(disable)
+    for(int i = 0; i < count; i ++) {
+        positions_novec[i].x += speeds_novec[i];
+        positions_novec[i].y += speeds_novec[i];
+    }
+    printf("   SoA (components), warm:       %f\n", timespec_measure(&start));
+
+
+
+    /* -- AoS -- */
+
+    timespec_gettime(&start);
+#pragma clang loop vectorize(enable)
+    for(int i = 0; i < count; i ++) {
         entities[i].p.x += entities[i].s;
         entities[i].p.y += entities[i].s;
+    }
+    printf("   AoS, cold:                    %f (V)\n", timespec_measure(&start));
+
+    timespec_gettime(&start);
+#pragma clang loop vectorize(enable)
+    for(int i = 0; i < count; i ++) {
+        entities[i].p.x += entities[i].s;
+        entities[i].p.y += entities[i].s;
+    }
+    printf("   AoS, warm:                    %f (V)\n", timespec_measure(&start));
+
+
+    /* No vectorization */
+    timespec_gettime(&start);
+#pragma clang loop vectorize(disable)
+    for(int i = 0; i < count; i ++) {
+        entities_novec[i].p.x += entities_novec[i].s;
+        entities_novec[i].p.y += entities_novec[i].s;
     }
     printf("   AoS, cold:                    %f\n", timespec_measure(&start));
 
     timespec_gettime(&start);
 #pragma clang loop vectorize(disable)
     for(int i = 0; i < count; i ++) {
-        entities[i].p.x += entities[i].s;
-        entities[i].p.y += entities[i].s;
+        entities_novec[i].p.x += entities_novec[i].s;
+        entities_novec[i].p.y += entities_novec[i].s;
     }
     printf("   AoS, warm:                    %f\n", timespec_measure(&start));
 
 
-    /* -- All attributes in one Entity struct, each separately allocated on heap */
+
+    /* -- Heap blocks -- */
+
     timespec_gettime(&start);
 #pragma clang loop vectorize(disable)
     for(int i = 0; i < count; i ++) {
@@ -136,25 +205,35 @@ void benchmark(int count) {
     printf("   Heap blocks, warm:            %f\n", timespec_measure(&start));
 
 
+    /* Cleanup */
     printf("-- Cleaning up data\n");
     free(x);
     free(y);
     free(s);
+    free(x_novec);
+    free(y_novec);
+    free(s_novec);
+
     free(positions);
     free(speeds);
     free(entities);
+
+    free(positions_novec);
+    free(speeds_novec);
+    free(entities_novec);
+
     for (int i = 0; i < count; i ++) {
         free(entity_ptrs[i]);
         free(garbage_ptrs[i]);
     }
     free(entity_ptrs);
     free(garbage_ptrs);
+
     printf("-- Benchmarks done\n");
 }
 
 int main(int argc, char *argv[]) {
     benchmark(100 * 1000);
-    benchmark(1000 * 1000);
     benchmark(1000 * 1000);
     benchmark(10 * 1000 * 1000);
     benchmark(50 * 1000 * 1000);
